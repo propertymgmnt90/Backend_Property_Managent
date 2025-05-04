78ddafeec34640f3e05fefbf4834c8a4188f4f74
propertymgmnt90 <propertymgmnt90@gmail.com>
2025-05-05 00:06:13 +0530

    Enable Cors Config

--- server.js
+++ server.js
@@ -375,11 +375,24 @@ app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 app.use(trackAPIStats);
 
 // CORS Configuration
+// app.use(cors({
+//   origin: [
+//     'http://localhost:4000',
+//     'http://localhost:5174',
+//     'http://localhost:5173',
+//   ],
+// // CORS Configuration
 app.use(cors({
   origin: [
     'http://localhost:4000',
     'http://localhost:5174',
     'http://localhost:5173',
+    'https://backend-property-managent.onrender.com',
+    // 'https://e2f2-110-226-127-135.ngrok-free.app/',
+    // 'https://buildestate.vercel.app',
+    // 'https://real-estate-website-admin.onrender.com',
+    // 'https://real-estate-website-backend-zfu7.onrender.com',
+    'https://admin-property-managent.vercel.app'
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'HEAD'],
